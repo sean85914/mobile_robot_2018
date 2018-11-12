4867a6512f24ac48e9475aa36e59b0f3f27d8d36
sean85914 <sean19960914@gmail.com>
2018-11-12 23:26:02 +0800

    reduce sleep time

--- src/demo_3/src/car_control.py
+++ src/demo_3/src/car_control.py
@@ -7,9 +7,9 @@ class Car_control(object):
 	def __init__(self):
 		self.pub_right_pwm = rospy.Publisher("right_pwm", Int16, queue_size = 10)
 		self.pub_left_pwm  = rospy.Publisher("left_pwm", Int16, queue_size = 10)
-		self.trim = rospy.get_param("~trim", 0)
+		self.trim = rospy.get_param("/fsm_node/trim", 0)
 		print "trim: ", self.trim
-		self.pwm = 100
+		self.pwm = 110
 		self.pwm_data = Int16()
 		rospy.sleep(3.0) # sleep 3.0 second to ensure topics connection
 	def stop_moving(self):
@@ -24,7 +24,7 @@ class Car_control(object):
 		self.stop_moving()
 		rospy.sleep(0.5)
 		self.pub_pwm(-self.pwm - self.trim, -self.pwm + self.trim)
-		rospy.sleep(2.0) # reverse 2.0 seconds
+		rospy.sleep(1.0) # reverse 1.0 seconds
 		self.pub_pwm(self.pwm/2, self.pwm)
 	def right_collision_recovery(self):
 		# since right collison, we have to reverse and turn left
@@ -32,7 +32,7 @@ class Car_control(object):
 		self.stop_moving()
 		rospy.sleep(0.5)
 		self.pub_pwm(-self.pwm - self.trim, -self.pwm + self.trim)
-		rospy.sleep(2.0) # reverse 2.0 seconds
+		rospy.sleep(1.0) # reverse 1.0 seconds
 		self.pub_pwm(self.pwm, self.pwm/2)
 	def pub_pwm(self, r_value, l_value):
 		pwm = Int16(r_value)
@@ -41,6 +41,7 @@ class Car_control(object):
 		pwm.data = l_value
 		print "left pwm: ", pwm.data
 		self.pub_left_pwm.publish(pwm)
+                rospy.sleep(1.0)
 	def shutdown(self):
 		pass	
 

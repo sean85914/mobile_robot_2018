1cf5ee75b7f0ca1923c14413ab7b8904ad1cc0a4
sean85914 <sean19960914@gmail.com>
2018-11-15 23:28:10 +0800

    add go_faster

--- src/demo_3/src/fsm_node.py
+++ src/demo_3/src/fsm_node.py
@@ -3,6 +3,7 @@
 import rospy
 import smach
 import smach_ros
+import RPi.GPIO as GPIO
 
 from std_msgs.msg import Bool
 from car_control import Car_control
@@ -10,22 +11,24 @@ from car_control import Car_control
 global controller
 controller = Car_control()
 
+'''
+  Touch sensor, down : 27
+  Touch sensor, left : 22
+  Touch sensor, right:  5
+'''
+# Pin mode setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.OUT) # photo collision
+GPIO.setup(22, GPIO.OUT) # left collision
+GPIO.setup(5, GPIO.OUT) # right collision
+
 class Go_straight(smach.State):
 	def __init__(self, start_time):
 		smach.State.__init__(self, outcomes=['right_collision', 'left_collision', 'photo_collision', 'time_out', 'keep_going'])
 		self.controller = controller
-		self.sub_right_collision = rospy.Subscriber('right_collision', Bool,
-		                                            self.right_collision_cb, queue_size = 1)
-		self.sub_left_collision = rospy.Subscriber('left_collision', Bool,
-		                                           self.left_collision_cb, queue_size = 1)
-		self.sub_photo_collision = rospy.Subscriber('photo_collision', Bool,
-							    self.photo_collision_cb, queue_size = 1)
 		self.sub_photo_state = rospy.Subscriber('photo_state', Bool,
                                                         self.photo_state_cb, queue_size = 1)
 		self.start_time = start_time.to_sec()
-		self.right_collision = False
-		self.left_collision  = False
-		self.photo_collision = False
 		self.photo_state     = False
 		self.times           = 0
 	def execute(self, userdata):
@@ -33,27 +36,25 @@ class Go_straight(smach.State):
 		if rospy.Time.now().to_sec() - self.start_time > 90:
 			self.controller.stop_moving()
 			return 'time_out'
-		if self.right_collision:
+		if GPIO.input(5):
 			return 'right_collision'
 		if self.times == 5: # go straight too long time
 			self.times = 0
 			rospy.loginfo("Go straight too long time, the car maybe blocked, go to left collision recovery")
 			return 'left_collision'
-		if self.left_collision:
+		if GPIO.input(22):
 			return 'left_collision'
-		if self.photo_collision:
+		if GPIO.input(27):
 			self.controller.stop_moving()
 			return 'photo_collision'
+		if self.photo_state:
+			self.controller.go_faster()
+			rospy.loginfo("Find light")
+			return 'keep_going'
 		else:
 			self.controller.go_straight()
 			self.times += 1
 			return 'keep_going'
-	def right_collision_cb(self, msg):
-		self.right_collision = msg.data
-	def left_collision_cb(self, msg):
-		self.left_collision = msg.data
-	def photo_collision_cb(self, msg):
-		self.photo_collision = msg.data
 	def photo_state_cb(self, msg):
 		self.photo_state = msg.data
 		
@@ -61,49 +62,41 @@ class Left_collision_recovery(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['left_collision', 'right_collision', 'photo_collision', 'go_straight', 'time_out'])
 		self.controller = controller
-		self.sub_left_collision  = rospy.Subscriber('left_collision', Bool,
-							   self.left_collision_cb, queue_size = 1)
-		self.sub_right_collision = rospy.Subscriber('right_collision', Bool,
-							    self.right_collision_cb, queue_size = 1)
-		self.sub_photo_collision = rospy.Subscriber('photo_collision', Bool,
-							    self.photo_collision_cb, queue_size = 1)
 		self.sub_photo_state = rospy.Subscriber('photo_state', Bool,
 							self.photo_state_cb, queue_size = 1)
-		self.left_collision  = False
-		self.right_collision = False
-		self.photo_collision = False
 		self.photo_state     = False
 		self.first_execute   = True
 		self.first_time      = None
+		self.str             = None
+		self.time            = None
 	def execute(self, userdata):
 		if self.first_execute:
-			self.controller.left_collision_recovery()
+			self.str = self.controller.left_collision_recovery()
 			self.first_time = rospy.Time.now().to_sec()
 			print "Left record time: ", self.first_time
 			self.first_execute = False
-		if rospy.Time.now().to_sec() - self.first_time > 1.0:
+			if self.str == 'spin':
+				self.time = 0.2
+			else:
+				self.time = 1.0
+		if rospy.Time.now().to_sec() - self.first_time > self.time:
 			self.first_execute = True
 			return 'time_out'
-		if self.left_collision:
+		if GPIO.input(22):
 			self.first_execute = True
 			return 'left_collision'
-		if self.right_collision:	
+		if GPIO.input(5):	
 			self.first_execute = True
 			return 'right_collision'
-		if self.photo_collision:
+		if GPIO.input(27):
 			self.controller.stop_moving()
 			self.first_execute = True
 			return 'photo_collision'
 		if self.photo_state:
 			self.first_execute = True
 			return 'go_straight'
+		rospy.sleep(0.1)
 		return 'left_collision'
-	def left_collision_cb(self, msg):
-		self.left_collision = msg.data
-	def right_collision_cb(self, msg):
-		self.right_collision = msg.data
-	def photo_collision_cb(self, msg):
-		self.photo_collision = msg.data
 	def photo_state_cb(self, msg):
 		self.photo_state = msg.data
 	
@@ -111,49 +104,41 @@ class Right_collision_recovery(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['left_collision', 'right_collision', 'photo_collision', 'go_straight', 'time_out'])
 		self.controller = controller
-		self.sub_left_collision  = rospy.Subscriber('left_collision', Bool,
-							    self.left_collision_cb, queue_size = 1)
-		self.sub_right_collision = rospy.Subscriber('right_collision', Bool,
-							    self.right_collision_cb, queue_size = 1)
-		self.sub_photo_collision = rospy.Subscriber('photo_collision', Bool,
-							    self.photo_collision_cb, queue_size = 1)
-		self.sub_photo_state     = rospy.Subscriber('photo_state', Bool,
-							    self.photo_state_cb, queue_size = 1)
-		self.left_collision  = False
-		self.right_collision = False
-		self.photo_collision = False
+		self.sub_photo_state = rospy.Subscriber('photo_state', Bool,
+							self.photo_state_cb, queue_size = 1)
 		self.photo_state     = False
 		self.first_execute   = True
-		self.first_time      = None            
+		self.first_time      = None
+		self.str             = None
+		self.time            = None
 	def execute(self, userdata):
 		if self.first_execute:
-			self.controller.right_collision_recovery()
+			self.str = self.controller.right_collision_recovery()
 			self.first_time = rospy.Time.now().to_sec()
 			print "Right record time: ", self.first_time
 			self.first_execute = False
-		if rospy.Time.now().to_sec() - self.first_time > 1.0:
+			if self.str == 'spin':
+				self.time = 0.2
+			else:
+				self.time = 1.0
+		if rospy.Time.now().to_sec() - self.first_time > self.time:
 			self.first_execute = True
 			return 'time_out'
-		if self.left_collision:
+		if GPIO.input(22):
 			self.first_execute = True
 			return 'left_collision'
-		if self.right_collision:
+		if GPIO.input(5):
 			self.first_execute = True
 			return 'right_collision'
 		if self.photo_state:
 			self.first_execute = True
 			return 'go_straight'
-		if self.photo_collision:
+		if GPIO.input(27):
 			self.controller.stop_moving()
 			self.first_execute = True
 			return 'photo_collision'
+		rospy.sleep(0.1)
 		return 'right_collision'
-	def left_collision_cb(self, msg):
-		self.left_collision = msg.data
-	def right_collision_cb(self, msg):
-		self.right_collision = msg.data
-	def photo_collision_cb(self, msg):
-		self.photo_collision = msg.data
 	def photo_state_cb(self, msg):
 		self.photo_state = msg.data
 		

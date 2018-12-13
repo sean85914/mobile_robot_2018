9f7560780243301fe128bfc12a1329e5601d323e
sean85914 <sean19960914@gmail.com>
2018-12-13 21:51:59 +0800

    complete some functions

--- src/demo_4/src/car_control_v2.py
+++ src/demo_4/src/car_control_v2.py
@@ -6,23 +6,33 @@ from std_msgs.msg import Int16
 
 class Car_control(object):
 	def __init__(self):
-		self.pub_right_pwm = rospy.Publisher("right_pwm", Int16, queue_size = 1)
-		self.pub_left_pwm  = rospy.Publisher("left_pwm", Int16, queue_size = 1)
+		self.pub_right_pwm = rospy.Publisher("right_pwm", Int16, queue_size = 5)
+		self.pub_left_pwm  = rospy.Publisher("left_pwm", Int16, queue_size = 5)
 		self.trim = rospy.get_param("~trim", -20)
+		print self.trim
 		self.pwm_data = Int16()
 		rospy.sleep(3.0)
 	# Stop moving
-	def stop_moving(self):
-		self.pub_pwm(0, 0)
+	def stop_moving(self, t):
+		self.pub_pwm(0, 0, t)
 	# Go straight
-	def go_straight(self, pwm, t)
+	def go_straight(self, pwm, t):
 		self.pub_pwm(pwm + self.trim, pwm - self.trim, t)
 	# Rotate in place
-	def rotate_in_place(self, direction, t)
+	def rotate_in_place(self, direction, t):
 		if direction == "CCW": # Counterclockwise
-			self.pub_pwm(60, 0, t)
+			self.pub_pwm(75, -75, t)
 		elif direction == "CW": # Clockwise
-			self.pub_pwm(0, 60, t)
+			self.pub_pwm(-75, 75, t)
+	# Reverse
+	def reverse(self, t):
+		self.pub_pwm(-80 - self.trim, -80 + self.trim, t)
+	# Turn
+	def turn(self, direction, t):
+		if direction == "Left" or direction == "left":
+			self.pub_pwm(100, 80, t)
+		elif direction == "Right" or direction == "right":
+			self.pub_pwm(80, 110, t)
 	# Publish pwm data
 	def pub_pwm(self, r_value, l_value, t):
 		ts = rospy.Time.now().to_sec()
@@ -31,15 +41,33 @@ class Car_control(object):
 			self.pub_right_pwm.publish(pwm_data)
 			pwm_data = l_value
 			self.pub_left_pwm.publish(pwm_data)
+			rospy.sleep(0.1)
+	# Delete class function
 	def __del__(self):
-		self.stop_moving()
-		rospy.sleep(2.0)
-
+		self.stop_moving(2.0)
+	# ROS shutdown function
+	def shutdown(self):
+		pass
 if __name__ == "__main__":
 	rospy.init_node("car_control_node")		
 	cc = Car_control()
-	cc.go_straight(100, 3)
-	cc.stop_moving()
-	rospy.sleep(1.0)
-	cc.rotate_in_place('CW', 0.5)
-	
+	rospy.on_shutdown(cc.shutdown)
+	#print "go straight"
+	#cc.go_straight(80, 2)
+	#print "stop"
+	#cc.stop_moving(1.0)
+	#print "rotate in place"
+	#cc.rotate_in_place('CW', 1)
+	#cc.stop_moving(1.0)
+	#cc.rotate_in_place('CCW', 1)
+	#cc.stop_moving(1.0)
+	#print "reverse"
+	#cc.reverse(3)
+	#print "turn left"
+	#cc.turn("Left", 1)
+	#print "stop"
+	#cc.stop_moving(1)
+	print "turn right"
+	cc.turn("Right", 1)
+	cc.stop_moving(1.0)
+	rospy.signal_shutdown("End")	

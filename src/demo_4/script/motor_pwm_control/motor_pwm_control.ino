de2fc3726bb279d9c1e9cbd2cef1f9396b8c880e
sean85914 <sean19960914@gmail.com>
2018-12-13 21:46:48 +0800

    finally work

--- src/demo_4/script/motor_pwm_control/motor_pwm_control.ino
+++ src/demo_4/script/motor_pwm_control/motor_pwm_control.ino
@@ -77,19 +77,19 @@ void loop()
   }
   // Left
   else if(pwm_r >= 0 and pwm_l <= 0){
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
     analogWrite(ENA, abs(pwm_l));
     analogWrite(ENB, pwm_r);
   }
   // Right
   else if(pwm_r <= 0 and pwm_l >= 0){
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
     analogWrite(ENA, pwm_l);
     analogWrite(ENB, abs(pwm_r));
   }

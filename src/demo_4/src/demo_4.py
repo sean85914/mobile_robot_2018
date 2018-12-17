1e234014a13fcb16fbe48febd8404f84c79d35d3
sean85914 <sean19960914@gmail.com>
2018-12-18 03:14:33 +0800

    fix

--- src/demo_4/src/demo_4.py
+++ src/demo_4/src/demo_4.py
@@ -24,6 +24,8 @@ class Demo(object):
 		self.cc = Car_control()
 		self.l_collision = None
 		self.r_collision = None
+		self.ts = None
+		self.thread_has_started = False
 	
 	def check_l_collision(self):
 		while 1:
@@ -43,23 +45,25 @@ class Demo(object):
 			if decision == 0:
 				self.cc.turn('right', 1.5)
 			else:
-				self.cc.rotate_in_place('CCW', 1.5)
+				self.cc.rotate_in_place('CCW', 100, 0.5)
 		if self.r_collision:
 			self.cc.reverse(1.5)
 			decision = random.randint(0, 1)
 			if decision == 0:
 				self.cc.turn('left', 1.5)
 			else:
-				self.cc.rotate_in_place('CW', 1.5)
+				self.cc.rotate_in_place('CW', 100, 0.5)
 
 	def start_thread(self):
 		self.thread_l.start()
 		self.thread_r.start()
+		self.ts = time.time()
+		self.thread_has_started = True
 	
 	def cb_state(self, msg):
 		self.state = msg.data
-		if self.state != self.last_state:
-			print "State changes from '{}' to '{}'".format(self.last_state, self.state)
+		if self.state != self.last_state and self.thread_has_started:
+			print "[Time: {}] State changes from '{}' to '{}'".format(time.time() - self.ts, self.last_state, self.state)
 		self.last_state = self.state
 
 	def approach_ball(self):
@@ -70,20 +74,31 @@ class Demo(object):
 			for i in range(4):
 				self.cc.rotate_in_place('CW', 100, 0.2)
 			print "Go straight again"
-			self.cc.go_straight(110, 1.5)
+			self.cc.go_straight(110, 2.0)
 			print "Rotate in place to find ball"
 			while self.state == "pi":
-				for i in range(4):
-					self.cc.rotate_in_place('CW', 100, 0.2)
-				if self.state == "pi":
-					print "Not found in first rotate, keep rotating with different direction..."
-					for i in range(9):
-						self.cc.rotate_in_place('CCW', 100, 0.2)
+				for i in range(2):
+					for j in range(4):
+						self.cc.rotate_in_place('CW', 100, 0.2)
+					if self.state == "pi":
+						print "Not found in first rotate, keep rotating with different direction..."
+						for j in range(4):
+							self.cc.rotate_in_place('CCW', 100, 0.2)
+					self.cc.go_straight(100, 1.0)
 			self.state = 'random'
+			self.last_state = 'pi'
+			print "[Time: {}] State changes from 'pi' to 'random'".format(time.time() - self.ts)
 			self.first = False
 		while not rospy.is_shutdown():
 			self.cc.go_straight(100, 2.0)
-			self,collision()
+			self.avoid()
+			direct_decide = random.randint(0, 1)
+			if direct_decide == 0:
+				direct = 'CCW'
+			else:
+				direct = 'CW'
+			self.cc.rotate_in_place(direct, 100, 0.5)
+			print "[Time: {}]".format(time.time() - self.ts)
 	
 	def shutdown(self):
 		print "Shutdown in progress"
